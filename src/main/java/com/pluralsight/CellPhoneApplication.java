c422a8b5a0a09f650d2e88ae665da4e38752909b
Aliza's <aliza.visuals@gmail.com>
2025-10-01 13:22:21 -0500

    Setters and Getters Added.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+
+        CellPhone defaultPhone = new CellPhone();
+
+        System.out.println("Default Owner: " + defaultPhone.getOwner());
+        System.out.println("Default Serial Number: " + defaultPhone.getSerialNumber());
+        System.out.println("Default Carrier: " + defaultPhone.getCarrier());
+        System.out.println("Default Model: " + defaultPhone.getModel());
+        System.out.println("Default Phone Number: " + defaultPhone.getPhoneNumber());
+
+
+        defaultPhone.setSerialNumber(1223434567);
+        defaultPhone.setModel("iPhone X");
+        defaultPhone.setOwner("Dana Wyatt Holmes");
+
+        System.out.println("\nAfter using setters:");
+        System.out.println("Owner: " + defaultPhone.getOwner());
+        System.out.println("Serial Number: " + defaultPhone.getSerialNumber());
+        System.out.println("Model: " + defaultPhone.getModel());
+        System.out.println("Owner: " + defaultPhone.getOwner());
+
+    }
 }
