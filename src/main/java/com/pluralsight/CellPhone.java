87b9325471cbc02a4376d44932ee4df5f70708ba
Aliza's <aliza.visuals@gmail.com>
2025-10-01 19:35:05 -0500

    Dail method added.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -7,6 +7,7 @@ public class CellPhone {
     private String phoneNumber;
     private String owner;
 
+
     public CellPhone() { // no parameters so it goes to default
         serialNumber = 023423435;
         model = "Nokia 215";
@@ -15,36 +16,37 @@ public class CellPhone {
         owner = "Aliza";
     }
 
+    public void dail (String phoneNumber){
+        System.out.println(owner + "'s phone is calling" + " " + phoneNumber);
+    }
+
+
+
+
+
+
+
     public int getSerialNumber() {
         return serialNumber; }
     public void setSerialNumber( int serialNumber ){
         this.serialNumber = serialNumber;}
-
-
     public String getModel() {
         return model; }
     public void setModel ( String model ){
         this.model = model; }
-
-
     public String getCarrier() {
         return carrier; }
     public void setCarrier(String carrier){
         this.carrier = carrier; }
-
     public String getPhoneNumber() {
         return phoneNumber; }
     public void setPhoneNumber( String phoneNumber ) {
         this.phoneNumber = phoneNumber ;
     }
-
     public String getOwner() {
         return owner; }
     public void setOwner(String owner) {
         this.owner = owner; }
-
-
-
 
 
 
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -3,24 +3,44 @@ package com.pluralsight;
 public class CellPhoneApplication {
     public static void main(String[] args) {
 
-        CellPhone defaultPhone = new CellPhone();
+        // CellPhone defaultPhone = new CellPhone();
+        CellPhone callerPhone = new CellPhone();
+        CellPhone calledPhone = new CellPhone();
 
-        System.out.println("Default Owner: " + defaultPhone.getOwner());
-        System.out.println("Default Serial Number: " + defaultPhone.getSerialNumber());
-        System.out.println("Default Carrier: " + defaultPhone.getCarrier());
-        System.out.println("Default Model: " + defaultPhone.getModel());
-        System.out.println("Default Phone Number: " + defaultPhone.getPhoneNumber());
+        callerPhone.setOwner("Dana Wyatt");
+        callerPhone.setPhoneNumber("855-555-111");
 
+        calledPhone.setOwner("Alex Johnson");
+        callerPhone.setPhoneNumber("934-555-2222");
 
-        defaultPhone.setSerialNumber(1223434567);
-        defaultPhone.setModel("iPhone X");
-        defaultPhone.setOwner("Dana Wyatt Holmes");
+        display(calledPhone);
+        display(callerPhone);
 
-        System.out.println("\nAfter using setters:");
-        System.out.println("Owner: " + defaultPhone.getOwner());
-        System.out.println("Serial Number: " + defaultPhone.getSerialNumber());
-        System.out.println("Model: " + defaultPhone.getModel());
-        System.out.println("Owner: " + defaultPhone.getOwner());
-
+        callerPhone.dail(calledPhone.getPhoneNumber());
+        calledPhone.dail(callerPhone.getPhoneNumber());
     }
+
+        public static void display(CellPhone phone) {
+            System.out.println("Owner: " + phone.getOwner());
+            System.out.println("Phone Number: " + phone.getPhoneNumber());
+            System.out.println("---------------------------");
+        }
+//
+//        System.out.println("Default Owner: " + defaultPhone.getOwner());
+//        System.out.println("Default Serial Number: " + defaultPhone.getSerialNumber());
+//        System.out.println("Default Carrier: " + defaultPhone.getCarrier());
+//        System.out.println("Default Model: " + defaultPhone.getModel());
+//        System.out.println("Default Phone Number: " + defaultPhone.getPhoneNumber());
+//        defaultPhone.setSerialNumber(1223434567);
+//        defaultPhone.setModel("iPhone X");
+//        defaultPhone.setOwner("Dana Wyatt Holmes");
+//
+//        System.out.println("\nAfter using setters:");
+//        System.out.println("Owner: " + defaultPhone.getOwner());
+//        System.out.println("Serial Number: " + defaultPhone.getSerialNumber());
+//        System.out.println("Model: " + defaultPhone.getModel());
+//        System.out.println("Owner: " + defaultPhone.getOwner());
+
+
+
 }
